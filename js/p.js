8b1738111bd900db0cf9d0f8b4f152ad0ed296f6
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-08-17 17:31:57 +0100

    buffed Ascension Power upgrade

--- js/p.js
+++ js/p.js
@@ -2466,10 +2466,10 @@ addLayer("p", {
 
         player.p.maxMult = (player.p.presmult).max(player.p.extraMult)
 
-        player.p.ascendMult = (player.p.maxMult).div(500).max(1).pow(0.375).floor().max(1).log2().pow(3).floor().times(2).add(2).pow(player.p.baseAscend)
+        player.p.ascendMult = (player.p.maxMult).div(500).max(1).pow(0.375).floor().max(1).log2().pow(10/3).floor().times(2).add(2).pow(player.p.baseAscend)
         player.p.ascendSpeed = (player.p.maxMult).div(250).max(1).pow(0.375).floor().div(2).max(1).log2().pow(0.95).max(1).add(1).pow(player.p.baseAscend)
-        player.p.ascendBoost = (player.p.maxMult).div(250).max(1).pow(0.5).floor().pow(0.225).max(1).log10().pow(2.5).times(10).round().div(10).add(1.2).pow(player.p.baseAscend)
-        player.p.ascendPower = (player.p.maxMult).max(1).log10().pow(0.05).max(1).times(a41)
+        player.p.ascendBoost = (player.p.maxMult).div(250).max(1).pow(0.5).floor().pow(0.225).max(1).log10().pow(2.75).times(10).round().div(10).add(1.2).pow(player.p.baseAscend)
+        player.p.ascendPower = (player.p.maxMult).max(1).log10().pow(0.0625).max(1).times(a41)
 
         // player.p.redSpd = new Decimal(1/2).times((Decimal.pow(1.67, ((player.p.redBuyAmt).div(5)))).div(10)).add(0.2)
         // player.p.orangeSpd = new Decimal(1/4).times((Decimal.pow(1.67, ((player.p.orangeBuyAmt).div(4)))).div(10)).add(0.1)
