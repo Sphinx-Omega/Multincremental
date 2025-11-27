93691399f10234dde35f4db704b3351dc58e3a85
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-11-27 04:59:10 +0000

    ccccc

--- js/inf.js
+++ js/inf.js
@@ -1611,7 +1611,7 @@ addLayer("inf", {
         44: {
             cost() { 
                 let sc = (this.bought().sub(4).max(0)).times(1.75).pow(1.1)
-                let cost = Decimal.pow(5,(this.bought())).max(1).times(new Decimal(80).pow(this.bought().div(2).add(1))).times(Decimal.pow(1e6,sc))
+                let cost = Decimal.pow(5,(this.bought())).max(1).times(new Decimal(75).pow(this.bought().div(2.5).add(1))).times(Decimal.pow(1e6,sc))
                 if(hasUpgrade("inf",161)) cost = cost.div(upgradeEffect("inf",161))
                 return cost 
             },
