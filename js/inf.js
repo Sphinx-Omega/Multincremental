b93e82cab9e9ee1bd8514824a60a20b33a8851d8
Sphinx-Omega <Death.Egg.MKIII@gmail.com>
2025-11-27 01:32:25 +0000

    another chance cost nerf

--- js/inf.js
+++ js/inf.js
@@ -1081,7 +1081,7 @@ addLayer("inf", {
         31: {
             cost() { 
                 let sc = player.inf.collchanceupg.max(28).sub(27).pow(0.15).times((this.bought().max(28).sub(27)).root(100))
-                let cost = Decimal.pow(10,(this.bought().times(3))).max(1).times(1e6).pow(0.75).pow(sc)
+                let cost = Decimal.pow(10,(this.bought().times(3))).max(1).times(5e7).pow(0.625).pow(sc)
                 if(hasUpgrade("inf",161)) cost = cost.div(upgradeEffect("inf",161))
                 return cost 
             },
